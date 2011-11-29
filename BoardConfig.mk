c550d7563a5c51040d5d7f91e23b9fa4baa69d8a
Sebastian Mauer <sebastian@n-unity.de>
2011-11-30 00:49:33 +0100

    Initial import

---
+++ BoardConfig.mk
@@ -0,0 +1,124 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+USE_CAMERA_STUB := true
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_LIBSECRIL_STUB := true
+
+# CPU
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOARD_PLATFORM := smdkv310
+TARGET_BOOTLOADER_BOARD_NAME := GT-N7000
+TARGET_BOARD_INFO_FILE := device/samsung/galaxynote/board-info.txt
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_OTA_ASSERT_DEVICE := galaxynote,GT-N7000
+
+TARGET_PROVIDES_INIT := true
+TARGET_PROVIDES_INIT_TARGET_RC := true
+TARGET_RECOVERY_INITRC := device/samsung/galaxynote/recovery.rc
+
+BOARD_NAND_PAGE_SIZE := 4096 -s 128
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 consoleblank=0
+TARGET_PREBUILT_KERNEL := device/samsung/galaxynote/kernel
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_USES_MMCUTILS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 893386752
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Releasetools
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/galaxynote/releasetools/galaxynote_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxynote/releasetools/galaxynote_img_from_target_files
+
+# Graphics (Mali 400)
+BOARD_EGL_CFG := device/samsung/galaxynote/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_USES_HWCOMPOSER := true
+BOARD_USES_LEGACY_EGL := true
+COMMON_GLOBAL_CFLAGS += -DUSES_LEGACY_EGL
+
+# Audio
+BOARD_USES_AUDIO_LEGACY := true
+BOARD_USE_YAMAHAPLAYER := true
+
+# Camera
+ifeq ($(USE_CAMERA_STUB),false)
+BOARD_CAMERA_LIBRARIES := libcamera
+endif
+
+# RIL
+BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
+
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_FM_DEVICE := si4709
+
+# GPS
+BOARD_USES_GPSWRAPPER := true
+
+# WiFi (BCM4330)
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_WLAN_DEVICE_REV            := bcm4330_b1
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_MODULE_PATH          := "/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/firmware/bcm4330_sta.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/firmware/bcm4330_aps.bin"
+WIFI_DRIVER_MODULE_NAME          :=  "dhd"
+WIFI_DRIVER_MODULE_ARG           :=  "firmware_path=/system/vendor/firmware/bcm4330_sta.bin nvram_path=/system/etc/nvram_net.txt"
+WIFI_BAND                        := 802_11_ABG
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 12
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+# Sensors
+#TARGET_USES_OLD_LIBSENSORS_HAL := true
+
+# Vibrator
+BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/galaxynote/vibrator/tspdrv.c
+
+# Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxynote/recovery/recovery_keys.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/galaxynote/recovery/graphics.c
+BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
+BOARD_HAS_NO_MISC_PARTITION := true 
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/galaxynote/shbootimg.mk
+
+-include device/samsung/galaxynote/BoardConfigVendor.mk
---
+++ galaxynote.mk
@@ -0,0 +1,172 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+DEVICE_PACKAGE_OVERLAYS := device/samsung/galaxynote/overlay
+
+# This device is hdpi.
+PRODUCT_AAPT_CONFIG := normal xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_LOCALES += xhdpi
+
+# Init files
+PRODUCT_COPY_FILES := \
+	device/samsung/galaxynote/lpm.rc:root/lpm.rc \
+	device/samsung/galaxynote/init.smdkv310.usb.rc:root/init.smdkv310.usb.rc \
+	device/samsung/galaxynote/init.smdkc210.rc:root/init.smdkc210.rc \
+	device/samsung/galaxynote/init.smdkv310.rc:root/init.smdkv310.rc \
+	device/samsung/galaxynote/ueventd.smdkv310.rc:root/ueventd.smdkv310.rc
+
+# APNs - REMOVE IF VENDOR CYANOGEN IS BACK
+PRODUCT_COPY_FILES += \
+	device/samsung/galaxynote/configs/apns-conf.xml:system/etc/apns-conf.xml \
+	device/samsung/galaxynote/configs/spn-conf.xml:system/etc/spn-conf.xml
+	
+# Touchscreen
+PRODUCT_COPY_FILES += \
+	device/samsung/galaxynote/configs/sec_ts_ics_bio.idc:system/usr/idc/sec_ts_ics_bio.idc
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+	device/samsung/galaxynote/keylayout/AVRCP.kl:/system/usr/keylayout/AVRCP.kl \
+	device/samsung/galaxynote/keylayout/Broadcom_Bluetooth_HID.kl:/system/usr/keylayout/Broadcom_Bluetooth_HID.kl \
+	device/samsung/galaxynote/keylayout/sec_jack.kl:/system/usr/keylayout/sec_jack.kl \
+	device/samsung/galaxynote/keylayout/sec_key.kl:/system/usr/keylayout/sec_key.kl \
+	device/samsung/galaxynote/keylayout/sec_touchkey.kl:/system/usr/keylayout/sec_touchkey.kl \
+	device/samsung/galaxynote/keylayout/sec_e-pen.kl:/system/usr/keylayout/sec_e-pen.kl \
+	device/samsung/galaxynote/keylayout/qwerty.kl:/system/usr/keylayout/qwerty.kl \
+	device/samsung/galaxynote/keylayout/Vendor_04E8_Product_7021.kl:/system/usr/keylayout/Vendor_04E8_Product_7021.kl
+	
+# Vold
+PRODUCT_COPY_FILES += \
+	device/samsung/galaxynote/configs/vold.fstab:system/etc/vold.fstab \
+	
+# Bluetooth configuration files
+PRODUCT_COPY_FILES += \
+	system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf
+
+# Wifi
+PRODUCT_COPY_FILES += \
+	device/samsung/galaxynote/configs/nvram_net.txt:system/etc/nvram_net.txt \
+	device/samsung/galaxynote/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	device/samsung/galaxynote/configs/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
+
+PRODUCT_PROPERTY_OVERRIDES := \
+	wifi.interface=eth0 \
+	wifi.supplicant_scan_interval=15
+
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
+
+# Gps
+PRODUCT_COPY_FILES += \
+	device/samsung/galaxynote/configs/gps.conf:system/etc/gps.conf \
+	device/samsung/galaxynote/configs/sirfgps.conf:system/etc/sirfgps.conf
+
+# Packages
+PRODUCT_PACKAGES := \
+    audio.primary.smdkv310 \
+    gps.smdkv310 \
+	com.android.future.usb.accessory
+
+# Charger
+#PRODUCT_PACKAGES += \
+#	charger \
+#	charger_res_images
+
+# Camera
+PRODUCT_PACKAGES += \
+	Camera
+
+# Sensors
+PRODUCT_PACKAGES += \
+	lights.smdkv310 \
+	sensors.smdkv310
+
+# Ril
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.ril_class=samsung \
+    ro.telephony.ril.v3=1 \
+    mobiledata.interfaces=pdp0,eth0,gprs,ppp0
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	static_busybox \
+	make_ext4fs \
+	setup_fs
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+	LiveWallpapers \
+	LiveWallpapersPicker \
+	VisualizationWallpapers \
+	librs_jni
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+	frameworks/base/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=131072
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=284 \
+	hwui.render_dirty_regions=false \
+    hwui.disable_vsync=true
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+# kernel modules for ramdisk
+RAMDISK_MODULES := $(addprefix device/samsung/galaxynote/modules/,bthid.ko dhd.ko gspca_main.ko j4fs.ko \
+	scsi_wait_scan.ko Si4709_driver.ko vibrator.ko)
+PRODUCT_COPY_FILES += $(foreach module,\
+	$(RAMDISK_MODULES),\
+	$(module):root/lib/modules/$(notdir $(module)))
+
+# other kernel modules not in ramdisk
+PRODUCT_COPY_FILES += $(foreach module,\
+	$(filter-out $(RAMDISK_MODULES),$(wildcard device/samsung/galaxynote/modules/*.ko)),\
+	$(module):system/lib/modules/$(notdir $(module)))
+
+# kernel modules for recovery ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxynote/modules/j4fs.ko:recovery/root/lib/modules/j4fs.ko
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+    LOCAL_KERNEL := device/samsung/galaxynote/kernel
+else
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+	
+# the kernel itself
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product-if-exists, vendor/samsung/galaxynote/galaxynote-vendor.mk)
