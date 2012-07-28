91157730848e43f561a1c4e75fc98a01c738424e
Andrew Dodd <atd7@cornell.edu>
2012-07-28 01:48:50 -0400

    n7000: Fork our init script

--- n7000.mk
+++ n7000.mk
@@ -76,4 +76,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
     $(LOCAL_PATH)/usr/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
 
+# init.smdk4210.rc
+PRODUCT_COPY_FILES := \
+    device/samsung/n7000/init.smdk4210.rc:root/init.smdk4210.rc
+
 $(call inherit-product-if-exists, vendor/samsung/n7000/n7000-vendor.mk)
