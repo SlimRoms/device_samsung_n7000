bf38e670f215dd2c65192c54edc26b2a475289b3
krarvind <krarvind@gmail.com>
2012-12-09 09:15:57 -0400

    slim specific changes and disable certain overlays

--- slim.mk
+++ slim.mk
@@ -15,20 +15,20 @@
 #
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GT-N7000
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7000/full_n7000.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7000
-PRODUCT_NAME := cm_n7000
+PRODUCT_NAME := slim_n7000
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-N7000
 
