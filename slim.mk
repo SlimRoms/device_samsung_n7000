d335af605601d4726046e42927ae6d36f7025c20
HardCorePawn <jared.yates@gmail.com>
2013-12-27 23:50:41 +0100

    n7000: removing torch settings
    Change-Id: I709aca2eac1dec0d1e854855c0cd526150a16ee3

--- slim.mk
+++ slim.mk
@@ -30,9 +30,6 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7000/full_n7000.mk)
 
-# Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7000
 PRODUCT_NAME := slim_n7000
